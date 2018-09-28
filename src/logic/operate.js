9fcf7456adee7fe47d22b207c415b8296c082387
Caroline Maiorana <cmaiorana@netdes.com>
2018-09-28 15:07:29 -0400

    fixed divide by zero bug

--- src/logic/operate.js
+++ src/logic/operate.js
@@ -1,19 +1,30 @@
 import Big from "big.js";
 
 export default function operate(numberOne, numberTwo, operation) {
-  const one = Big(numberOne || "0");
-  const two = Big(numberTwo || "0");
-  if (operation === "+") {
-    return one.plus(two).toString();
-  }
-  if (operation === "-") {
-    return one.minus(two).toString();
-  }
-  if (operation === "x") {
-    return one.times(two).toString();
-  }
-  if (operation === "÷") {
-    return one.div(two).toString();
-  }
-  throw Error(`Unknown operation '${operation}'`);
+    const one = Big(numberOne || "0");
+    const two = Big(numberTwo || "0");
+    if (operation === "+") {
+        // console.log('plus');
+        return one.plus(two).toString();
+    }
+    if (operation === "-") {
+        // console.log('minus');
+        return one.minus(two).toString();
+    }
+    if (operation === "x") {
+        // console.log('times');
+        return one.times(two).toString();
+    }
+    if (operation === "÷") {
+        // console.log('divided by');
+        if (+two.toString() === 0) {
+            console.log('cannot divide by zero');
+            return NaN;
+
+        } else {
+            return one.div(two).toString();
+
+        }
+    }
+    throw Error(`Unknown operation '${operation}'`);
 }
